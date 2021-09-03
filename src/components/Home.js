5c2e9bdc52b467f195603fcfef5d41cceecc07d2
Nabeel Danish <nabeelben@gmail.com>
2021-09-03 18:40:23 +0500

    Single Post Component Added
    - Added the singlePost page, with slug routing
    - Fetches data from Sanity to display correct content block

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
                 className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8"
             >
                 <h1 
-                    className="text-6xl text-white font-bold cursive leading-none lg:leading-snug home-name"
+                    className="text-6xl text-white font-bold cursive leading-none lg:leading-snug home-name user-select:none"
                 >
                     Hello. I'm Nabeel.
                 </h1>
---
+++ studio/schemas/project.js
@@ -0,0 +1,53 @@
+export default {
+    name: "project",
+    title: "Project",
+    type: "document",
+    fields: [
+        {
+            name: "title",
+            type: "string",
+
+        },
+        {
+            name: "date",
+            type: "datetime",
+        },
+        {
+            name: "place",
+            type: "string",
+        },
+        {
+            name: "description",
+            type: "text",
+        },
+        {
+            name: "projectType",
+            title: "Project Type",
+            type: "string",
+            options: {
+                list: [
+                    { value: "personal", title: "Personal"},
+                    { value: "academic", title: "Academic"},
+                    { value: "open_source", title: "Open Source"},
+                    { value: "corporate", title: "Corporate"},
+                ]
+            },
+        },
+        {
+            name: "link",
+            type: "url",
+        },
+        {
+            name: "tags",
+            type: "array",
+            of: [
+                {
+                    type:"string",
+                },
+            ],
+            options: {
+                layout: "tags",
+            },
+        }
+    ],
+};
